89be47d1d6e377f4aeedaf1f86ff8e23db5661c9
cartosys <plaster1@gmail.com>
2020-05-16 13:01:14 -0400

    revamped ERC20 Factory

--- assets/sol/ERC20-contract-factory/ERC20Factory.sol
+++ assets/sol/ERC20-contract-factory/ERC20Factory.sol
@@ -1,28 +1,26 @@
-import "./EIP20.sol";
+import "./ERC20Full.sol";
 
-pragma solidity >=0.4.20;
+pragma solidity ^0.6.2;
 
 
 contract EIP20Factory {
 
     mapping(address => address[]) public created;
     mapping(address => bool) public isEIP20; //verify without having to do a bytecode check.
-    bytes public EIP20ByteCode; // solhint-disable-line var-name-mixedcase
-
+    
     constructor() public {
         //upon creation of the factory, deploy a EIP20 (parameters are meaningless) and store the bytecode provably.
-        address verifiedToken = createEIP20(10000, "Verify Token", 3, "VTX");
+        createEIP20(10000, "Verify Token", 3, "VTX");
     }
 
     function createEIP20(uint256 _initialAmount, string memory _name, uint8 _decimals, string memory _symbol)
         public
     returns (address) {
 
-        EIP20 newToken = (new EIP20(_initialAmount, _name, _decimals, _symbol));
+        ERC20Full newToken = (new ERC20Full(msg.sender, _name, _symbol, _decimals, _initialAmount));
         created[msg.sender].push(address(newToken));
         isEIP20[address(newToken)] = true;
         //the factory will own the created tokens. You must transfer them.
-        newToken.transfer(msg.sender, _initialAmount);
         return address(newToken);
     }
 }
--- assets/sol/ERC20-contract-factory/ERC20Full.sol
+++ assets/sol/ERC20-contract-factory/ERC20Full.sol
@@ -1,1 +1,19 @@
-ERC20Full.sol
+pragma solidity ^0.6.2;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20Burnable.sol";
+
+
+contract ERC20Full is ERC20, ERC20Burnable {
+	
+	constructor(
+	    address userAddress,
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        uint256 _initialAmount
+    ) ERC20(_name, _symbol) public {
+	    _mint(userAddress, _initialAmount);
+	    _setupDecimals(_decimals);
+	}
+}
