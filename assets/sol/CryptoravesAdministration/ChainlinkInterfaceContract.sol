cd4e149cb5bac13f1297dde739bba201e6b62b66
cartosys <plaster1@gmail.com>
2020-05-17 21:26:52 -0400

    managed 1155s

--- assets/sol/CryptoravesAdministration/ChainlinkInterfaceContract.sol
+++ assets/sol/CryptoravesAdministration/ChainlinkInterfaceContract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.2;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol"; //release-v3.0.0
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol"; //release-v3.0.0
 
-contract TweetValidation {
+contract ContentValidation {
 	
 	using SafeMath for uint256;
     using Address for address;
@@ -17,6 +17,7 @@ contract TweetValidation {
     * Require msg.sender to be validator
     */
     modifier onlyValidator () {
+      // can we pull from a Chainlink mapping?
       require(_validators[msg.sender], 'Sender is not a validator.');
       _;
     }
@@ -45,12 +46,14 @@ contract TweetValidation {
     /*
     * check incoming parsed Tweet data for valid command
     * @param twitterAddressFrom The Twitter ID of the Tweet author
+    * @param fromImgUrl The Twitter img of user
     * @param param1 first possible param to ckeck
     * @param param2 second possible param to ckeck
     * @param param3 third possible param to ckeck
     */ 
     function validateCommand(
-        uint256 twitterAddressFrom, 
+        uint256 twitterIdFrom,
+        uint256 fromImageUrl,
         string memory param1, 
         string memory param2, 
         string memory param3
