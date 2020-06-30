f274cd985e112b8ae4ceb35c840a3f47b4f1167e
cartosys <plaster1@gmail.com>
2020-06-30 15:12:45 -0400

    new file

---
+++ contracts/ERC721-contract-factory/ERC721Full.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.6.10;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol"; //release-v3.0.0
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol"; //release-v3.0.0
+
+contract ERC721Full is IERC721Metadata, IERC721Enumerable,  ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor(address userAddress, string memory name, string memory symbol) ERC721(name, symbol) public {
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(userAddress, newItemId);
+    }
+    
+    function mint(address to, string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(to, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
