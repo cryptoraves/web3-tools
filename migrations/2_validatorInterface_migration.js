8f9c5e78cd37251cd90a82179f811870ff932016
cartosys <plaster1@gmail.com>
2020-07-07 14:46:39 -0400

    compiled contracts

--- migrations/2_validatorInterface_migration.js
+++ migrations/2_validatorInterface_migration.js
@@ -1,4 +1,4 @@
-const ValidatorInterfaceContract = artifacts.require('./ValidatorInterfaceContract.sol')
+const ValidatorInterfaceContract = artifacts.require('ValidatorInterfaceContract')
 
 const imgUrl = 'https://i.picsum.photos/id/99/200/200.jpg'
 const originAddr = '0x0000000000000000000000000000000000000000'
@@ -6,9 +6,9 @@ const originAddr = '0x0000000000000000000000000000000000000000'
 module.exports = function (deployer, network, accounts) {
   
   deployer.then(async () => {
-    
+    console.log(ValidatorInterfaceContract)
     await deployer.deploy(ValidatorInterfaceContract, imgUrl, originAddr)
-    const instance = await MyCryptoravesToken.deployed()
+    const instance = await ValidatorInterfaceContract.deployed()
         
     console.log('\n*************************************************************************\n')
     console.log('ValidatorInterfaceContract Contract Address: '+instance.address)
--- remapGithubPaths.sh
+++ remapGithubPaths.sh
@@ -1,4 +1,3 @@
 rel_path=$(dirname $(realpath $0))
-echo $rel_path
 
 find . -type f -name "*.sol" -exec sed -i'' -e 's#https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master#'${HOME}'/www/openzeppelin-contracts#g' {} +
