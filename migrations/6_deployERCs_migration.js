d3bae8a444aa62364e53d9ca27159de672cc751e
cartosys <plaster1@gmail.com>
2020-07-14 15:50:01 -0400

    deploy erc20 & 721

---
+++ migrations/6_deployERCs_migration.js
@@ -0,0 +1,28 @@
+const ERC20Full = artifacts.require('ERC20Full')
+const ERC721Full = artifacts.require('ERC721Full')
+
+const ethers = require('ethers')
+
+module.exports = function (deployer, network, accounts) {
+  
+  deployer.then(async () => {
+
+    await deployer.deploy(ERC20Full, accounts[0], 'TokenX', 'TKX', 18, ethers.utils.parseUnits('1000000000',18))
+    const instance = await ERC20Full.deployed()
+        
+    console.log('\n*************************************************************************\n')
+    console.log('ERC20 Address: '+instance.address)
+    console.log('\n*************************************************************************\n')
+  })
+
+  deployer.then(async () => {
+  	
+    await deployer.deploy(ERC721Full, accounts[0], 'TokenY', 'TKY')
+    const instance = await ERC721Full.deployed()
+        
+    console.log('\n*************************************************************************\n')
+    console.log('ERP721 Address: '+instance.address)
+    console.log('\n*************************************************************************\n')
+  })
+  
+}
--- test/cryptoravesToken.test.js
+++ test/cryptoravesToken.test.js
@@ -1,8 +1,19 @@
 const CryptoravesToken = artifacts.require("CryptoravesToken");
 
+const ERC20Full = artifacts.require('ERC20Full')
+const ERC721Full = artifacts.require('ERC721Full')
+
+let erc20, erc721
+
 contract("CryptoravesToken", async accounts => {
-  it("should Launch CryptoravesToken Contract", async () => {
-    let instance = await CryptoravesToken.deployed()
+  it("should Launch ERC20 & ERC721 contract", async () => {
+    let erc20Instance = await ERC20Full.deployed()
+    let erc721Instance = await ERC721Full.deployed()
+
+    
+    console.log(erc20Instance.address)
+    console.log(erc721Instance.address)
+
     //let balance = await instance.getBalance.call(accounts[0]);
     //assert.equal(balance.valueOf(), 10000);
     //console.log(instance)
