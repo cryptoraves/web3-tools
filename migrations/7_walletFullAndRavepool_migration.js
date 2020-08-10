f2659e10cf29fb7d8089b4e3c5d18ce4569a1c6f
cartosys <plaster1@gmail.com>
2020-08-10 11:45:09 -0400

    ravepool functionality testing

--- migrations/7_walletFullAndRavepool_migration.js
+++ migrations/7_walletFullAndRavepool_migration.js
@@ -3,7 +3,7 @@ const WalletFull = artifacts.require('WalletFull')
 module.exports = function (deployer, network, accounts) {
   
   deployer.then(async () => {
-    await deployer.deploy(accounts[0], {from: accounts[0]})
+    await deployer.deploy(accounts[0], {from: accounts[1]})
     const instance = await WalletFull.deployed()
         
     console.log('\n*************************************************************************\n')
