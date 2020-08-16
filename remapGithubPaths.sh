9fe0afad642959c43bed88a7cdff2baac4c54466
cartosys <plaster1@gmail.com>
2020-08-16 17:45:51 -0400

    refactor pathname algorith

--- remapGithubPaths.sh
+++ remapGithubPaths.sh
@@ -1,3 +1,4 @@
 rel_path=$(dirname $(realpath $0))
 
 find . -type f -name "*.sol" -exec sed -i'' -e 's#https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master#'${HOME}'/www/openzeppelin-contracts#g' {} +
+find . -type f -name "*.sol" -exec sed -i'' -e 's#https://github.com/OpenZeppelin/openzeppelin-contracts#'${HOME}'/www/openzeppelin-contracts#g' {} +
