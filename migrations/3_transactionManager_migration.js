cf11e7f5feded8548fdeb792da4068197f7028bd
cartosys <plaster1@gmail.com>
2020-08-16 16:16:57 -0400

    changed tokenManagement to transactionManagement

--- migrations/3_transactionManager_migration.js
+++ migrations/3_transactionManager_migration.js
@@ -1,4 +1,4 @@
-const TokenManagement = artifacts.require('TokenManagement')
+const TransactionManagement = artifacts.require('TransactionManagement')
 
 const imgUrl = 'https://i.picsum.photos/id/99/200/200.jpg'
 const originAddr = '0x0000000000000000000000000000000000000000'
@@ -6,11 +6,11 @@ const originAddr = '0x0000000000000000000000000000000000000000'
 module.exports = function (deployer) {
   
   deployer.then(async () => {
-    await deployer.deploy(TokenManagement, imgUrl, originAddr, originAddr)
-    const instance = await TokenManagement.deployed()
+    await deployer.deploy(TransactionManagement, imgUrl, originAddr, originAddr)
+    const instance = await TransactionManagement.deployed()
         
     console.log('\n*************************************************************************\n')
-    console.log('TokenManagement Contract Address: '+instance.address)
+    console.log('TransactionManagement Contract Address: '+instance.address)
     console.log('\n*************************************************************************\n')
   })
 }
