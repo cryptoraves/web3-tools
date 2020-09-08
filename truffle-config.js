395fbfbe33fdd02539eb15065a3a092d14207cd5
cartosys <plaster1@gmail.com>
2020-09-08 12:44:44 -0400

    matic testnet

--- truffle-config.js
+++ truffle-config.js
@@ -64,12 +64,12 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
      rinkeby: {
-        provider: () => new HDWalletProvider(privateKey, 'https://rinkeby.infura.io/v3/'+infuraKey),
-        network_id: 4,       // Ropsten's id
-        gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+       provider: () => new HDWalletProvider(privateKey, 'https://rinkeby.infura.io/v3/'+infuraKey),
+       network_id: 4,       // Ropsten's id
+       gas: 5500000,        // Ropsten has a lower block limit than mainnet
+       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
      },
 
       skaleTest: {
@@ -107,7 +107,7 @@ module.exports = {
        docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
        settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
-          enabled: true,
+          enabled: true
           runs: 200
         },
         //evmVersion: "byzantium"
