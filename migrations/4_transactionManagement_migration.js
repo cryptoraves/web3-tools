c177e3c3b18ed16377bf39ad2f8277ef4241b542
cartosys <plaster1@gmail.com>
2020-09-10 14:33:04 -0400

    adjusted migrations

--- migrations/4_transactionManagement_migration.js
+++ migrations/4_transactionManagement_migration.js
@@ -1,4 +1,5 @@
 const TransactionManagement = artifacts.require('TransactionManagement')
+const AdminToolsLibrary = artifacts.require('AdminToolsLibrary')
 
 const imgUrl = 'https://i.picsum.photos/id/99/200/200.jpg'
 const originAddr = '0x0000000000000000000000000000000000000000'
@@ -9,6 +10,8 @@ console.log(size)
 module.exports = function (deployer) {
   
   deployer.then(async () => {
+  	await deployer.deploy(AdminToolsLibrary)
+  	await deployer.link(AdminToolsLibrary, TransactionManagement)
     await deployer.deploy(TransactionManagement, imgUrl, originAddr, originAddr)
     const instance = await TransactionManagement.deployed()
         
--- migrations/7_validatorInterface_migration.js
+++ migrations/7_validatorInterface_migration.js
@@ -1,4 +1,5 @@
 const ValidatorInterfaceContract = artifacts.require('ValidatorInterfaceContract')
+const AdminToolsLibrary = artifacts.require('AdminToolsLibrary')
 
 const imgUrl = 'https://i.picsum.photos/id/99/200/200.jpg'
 const originAddr = '0x0000000000000000000000000000000000000000'
@@ -7,6 +8,8 @@ module.exports = function (deployer) {
   
   deployer.then(async () => {
   	
+  	await deployer.deploy(AdminToolsLibrary)
+  	await deployer.link(AdminToolsLibrary, ValidatorInterfaceContract)
     await deployer.deploy(ValidatorInterfaceContract, imgUrl, originAddr, originAddr)
     const instance = await ValidatorInterfaceContract.deployed()
         
