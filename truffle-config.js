10b05e701d5079213060139c684fd0502d049d44
cartosys <plaster1@gmail.com>
2020-09-11 13:50:51 -0400

    timeout issue on matic

--- truffle-config.js
+++ truffle-config.js
@@ -69,7 +69,7 @@ module.exports = {
        gas: 5500000,        // Ropsten has a lower block limit than mainnet
        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+       skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
       },
 
       skaleTest: {
@@ -84,8 +84,7 @@ module.exports = {
         provider: () => new HDWalletProvider(privateKey, `https://rpc-mumbai.matic.today`),
         network_id: 80001,
         confirmations: 2,
-        timeoutBlocks: 200,
-        skipDryRun: true
+        timeoutBlocks: 200
       },
     // Useful for private networks
     // private: {
