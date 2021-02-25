46feb705d9f00c3d094df32e77c6a24be776e87d
cartosys <bryan@cryptoraves.space>
2021-02-25 16:13:10 -0500

    launching server side

--- 10_performance_migration.js
+++ 10_performance_migration.js
@@ -1,6 +1,10 @@
 const ERC20Full = artifacts.require('ERC20Full')
 const ERC721Full = artifacts.require('ERC721Full')
 
+const TransactionManagement = artifacts.require("TransactionManagement")
+const TokenManagement = artifacts.require("TokenManagement")
+const ValidatorInterfaceContract = artifacts.require("ValidatorInterfaceContract")
+
 const ethers = require('ethers')
 
 const fs = require('fs');
@@ -24,11 +28,20 @@ const erc721s = [
 	'NFTA5','NFTA6','NFTA7','NFTA8','NFTA9','NFTB1','NFTB2','NFTB3','NFTB4','NFTB5'
 ]
 
-let account = output = ''
-let balance = 0
+let account = output = instance = instanceTokenManagement = appr = validatorInstance = ''
+let balance = Erc1155tokenID = 0
 
 module.exports = function (deployer, network, accounts) {
-  
+	deployer.then(async () => {
+		  validatorInstance = await ValidatorInterfaceContract.at('0x97ef4a7B0c740e372f1F7D0C2281BdEeBd786e30')
+		  let instanceTransactionManagement = await TransactionManagement.at(
+		    await validatorInstance.getTransactionManagementAddress()
+		  )
+		  // Launch an ERC20 and deposit some to instantiate onto Cryptoraves. Then send some to Twitter user L1
+		  instanceTokenManagement = await TokenManagement.at(
+		    await instanceTransactionManagement.getTokenManagementAddress()
+		  )
+	})
   //ERC20's
   deployer.then(async () => {
   	
@@ -37,18 +50,15 @@ module.exports = function (deployer, network, accounts) {
   		account = ethers.Wallet.createRandom().address
   		amount = getRandomInt(10000) * getRandomInt(10000)
 
-  		await deployer.deploy(ERC20Full, account, 'Token'+token, token, 18, ethers.utils.parseUnits(amount.toString(),18))
-	    const instance = await ERC20Full.deployed()
+  		await deployer.deploy(ERC20Full, accounts[0], 'Token'+token, token, 18, ethers.utils.parseUnits(amount.toString(),18))
+	    instance = await ERC20Full.deployed()
 	        
-	    console.log('\n*************************************************************************\n')
-	    console.log('ERC20 Address:',instance.address)
-	    console.log('To Account:',account)
-	    console.log('Amount:',amount)
-	    console.log('\n*************************************************************************\n')
+	    balance = await instance.balanceOf(accounts[0])
 
-	    balance = await instance.balanceOf(account)
-
-	    output = 'Account: '+account+' Token: '+token+' Balance: '+ethers.utils.formatUnits(balance.toString(), 18)+"\n"
+	    appr = await instance.approve(instanceTokenManagement.address, ethers.utils.parseEther(amount.toString()))
+	    Erc1155tokenID = await instanceTokenManagement.deposit(ethers.utils.parseEther(amount.toString()), instance.address, 20, true)
+	    Erc1155tokenID = Erc1155tokenID.logs[0]['args']['cryptoravesTokenId'].toString()
+	    output = 'Account: '+accounts[0]+' Token: '+token+' Token Address: '+instance.address+' Balance: '+ethers.utils.formatUnits(balance.toString(), 18)+' TokenID: '+Erc1155tokenID+"\n"
 	  	await fs.appendFile(outputPath, output, function (err) {
 	  		console.log(output)
 			if (err) throw err
@@ -58,24 +68,28 @@ module.exports = function (deployer, network, accounts) {
 
   //ERC721's
   deployer.then(async () => {
-  	
+  	let tokenID = 0
   	for await (token of erc721s){
   		
   		account = ethers.Wallet.createRandom().address
   		
-  		await deployer.deploy(ERC721Full, account, 'Token'+token, token)
-	    const instance = await ERC721Full.deployed()
+  		await deployer.deploy(ERC721Full, accounts[0], 'Token'+token, token)
+	    instance = await ERC721Full.deployed()
 	        
-	    await instance.mint(account, 'http:/abc.com')
+	    await instance.mint(accounts[0], 'http:/abc.com')
 	    
-	    console.log('\n*************************************************************************\n')
-	    console.log('ERC721 Address:',instance.address)
-	    console.log('To Account:',account)
-	    console.log('\n*************************************************************************\n')
+	    balance = await instance.balanceOf(accounts[0])
 
-	    balance = await instance.balanceOf(account)
+	    if ( tokenID == 0){
+	    	tokenID=1
+	    }else{
+	    	tokenID=0
+	    }
 
-	    output = 'Account: '+account+' Token: '+token+' Balance: '+balance.toString()+"\n"
+	    appr = await instance.approve(instanceTokenManagement.address, tokenID)
+	    Erc1155tokenID = await instanceTokenManagement.deposit(tokenID, instance.address, 721, true)
+	    Erc1155tokenID = Erc1155tokenID.logs[0]['args']['cryptoravesTokenId'].toString()
+	    output = 'Account: '+accounts[0]+' Token: '+token+' Token Address: '+instance.address+' Balance: '+balance.toString()+' TokenID: '+Erc1155tokenID+"\n"
 	  	await fs.appendFile(outputPath, output, function (err) {
 	  		console.log(output)
 			if (err) throw err
