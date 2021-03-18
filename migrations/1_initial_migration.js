406b00d2a85f887f161fe51a96d88a0f4cc9e1a4
cartosys <bryan@cryptoraves.space>
2021-03-18 14:01:09 -0400

    migration cleanup

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,14 @@
 const Migrations = artifacts.require("Migrations");
 
+const fs = require('fs');
+const outputPath = '/tmp/contractAddresses.json'
+
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+	deployer.deploy(Migrations);
+	deployer.then(async () => {
+		await fs.appendFile(outputPath, '{', function (err) {
+		  if (err) throw err
+
+		})
+	})
 };
