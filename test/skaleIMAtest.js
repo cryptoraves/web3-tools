8b836045d330bc48063f889d6403d3a1395d12a0
cartosys <bryan@cryptoraves.space>
2021-12-18 12:12:47 -0500

    added eth2 testnet

--- test/skaleIMAtest.js
+++ test/skaleIMAtest.js
@@ -1,21 +1,22 @@
 //import { IMA } from '@skalenetwork/ima-js';
-const IMA = require('@skalenetwork/ima-js');
-console.log(config.networks)
+/*const IMA = require("@skalenetwork/ima-js");
+console.log(config.networks);
 //import mainnetAbi from './mainnetAbi.json'; // your local sources
 //import schainAbi from './schainAbi.json'; // your local sources
 
-const MAINNET_ENDPOINT = 'http://127.0.0.1:8545';
-const SCHAIN_ENDPOINT = 'http://127.0.0.1:15000';
+const MAINNET_ENDPOINT = "http://127.0.0.1:8545";
+const SCHAIN_ENDPOINT = "http://127.0.0.1:15000";
 
 const mainnetWeb3 = new Web3(MAINNET_ENDPOINT);
 const sChainWeb3 = new Web3(SCHAIN_ENDPOINT);
 
 let ima = new IMA(mainnetWeb3, sChainWeb3, mainnetAbi, sChainAbi);
 
-contract("SkaleIMA", async (accounts) => {
+contract("SkaleIMA", async accounts => {
   it("skale deposit eth test", async () => {
-    console.log('using network',config.network)
-    console.log(await config.networks[config.network].provider())
+    console.log("using network", config.network);
+    console.log(await config.networks[config.network].provider());
     //console.log(await web3.currentProvider)
-  })
-})
+  });
+});
+*/
--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,16 @@
  *
  */
 
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const fs = require('fs');
-const privateKey = fs.readFileSync(".prvkey").toString().trim();
-const infuraKey = fs.readFileSync(".infuraKey").toString().trim();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const privateKey = fs
+  .readFileSync(".prvkey")
+  .toString()
+  .trim();
+const infuraKey = fs
+  .readFileSync(".infuraKey")
+  .toString()
+  .trim();
 //const fs = require('fs');
 //const mnemonic = fs.readFileSync(".secret").toString().trim();
 //const infuraKey = fs.readFileSync(".infuraKey").toString().trim();
@@ -44,98 +50,137 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-      gas: 20000000,
-     },
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+      gas: 20000000
+    },
 
     // Another network with more advanced options...
     // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     // },
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-      rinkeby: {
-       provider: () => new HDWalletProvider(privateKey, 'https://rinkeby.infura.io/v3/'+infuraKey),
-       network_id: 4,       // Ropsten's id
-       gas: 29970705,        // Ropsten has a lower block limit than mainnet
-       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-       timeoutBlocks: 400,  // # of blocks before a deployment times out  (minimum/default: 50)
-       skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
-      },
-      goerli: {
-       provider: () => new HDWalletProvider(privateKey, 'https://goerli.infura.io/v3/'+infuraKey),
-       network_id: 5,       // goerli's id
-       gas: 5500000,        // goerli has a lower block limit than mainnet
-       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          "https://rinkeby.infura.io/v3/" + infuraKey
+        ),
+      network_id: 4, // Ropsten's id
+      gas: 29970705, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 400, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
+    },
+    goerli: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          "https://goerli.infura.io/v3/" + infuraKey
+        ),
+      network_id: 5, // goerli's id
+      gas: 5500000, // goerli has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200 // # of blocks before a deployment times out  (minimum/default: 50)
+    },
 
-      skaleTest: {
-        provider: () => new HDWalletProvider(privateKey, 'https://dev-testnet-v1-0.skalelabs.com'),
-        gasPrice: 0,
-        gas: 40000000,
-        network_id: "*",
-        ChainID: 344435,
-        skipDryRun: true
-      },
-      skaleTestBob: {
-        provider: () => new HDWalletProvider(privateKey, 'https://dev-testnet-v1-1.skalelabs.com'),
-        gasPrice: 0,
-        gas: 40000000,
-        network_id: "*",
-        timeout: 100000,
-        ChainID: 344435,
-        skipDryRun: true
-      },
-      maticTest: {
-        provider: () => new HDWalletProvider(privateKey, `https://matic-mumbai.chainstacklabs.com`),
-        network_id: 80001,
-        skipDryRun: true,
-        enableTimeouts: false,
-        before_timeout: 240000
-      },
-      matic: {
-        provider: () => new HDWalletProvider(privateKey, `https://matic-mainnet.chainstacklabs.com`),
-        network_id: 137,
-        confirmations: 2,
-        timeoutBlocks: 400
-      },
-      arbitrumTest: {
-        provider: () => new HDWalletProvider(privateKey, `https://rinkeby.arbitrum.io/rpc`),
-        network_id: 421611,
-        gas: 40000000
-      },
-      arbitrumTest2: {
-        provider: () => new HDWalletProvider(privateKey, `https://kovan5.arbitrum.io/rpc`),
-        network_id: "*"
-      },
+    skaleTest: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          "https://dev-testnet-v1-0.skalelabs.com"
+        ),
+      gasPrice: 0,
+      gas: 40000000,
+      network_id: "*",
+      ChainID: 344435,
+      skipDryRun: true
+    },
+    skaleTestBob: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          "https://dev-testnet-v1-1.skalelabs.com"
+        ),
+      gasPrice: 0,
+      gas: 40000000,
+      network_id: "*",
+      timeout: 100000,
+      ChainID: 344435,
+      skipDryRun: true
+    },
+    maticTest: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          `https://matic-mumbai.chainstacklabs.com`
+        ),
+      network_id: 80001,
+      skipDryRun: true,
+      enableTimeouts: false,
+      before_timeout: 240000
+    },
+    matic: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKey,
+          `https://matic-mainnet.chainstacklabs.com`
+        ),
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 400
+    },
+    arbitrumTest: {
+      provider: () =>
+        new HDWalletProvider(privateKey, `https://rinkeby.arbitrum.io/rpc`),
+      network_id: 421611,
+      gas: 40000000
+    },
+    arbitrumTest2: {
+      provider: () =>
+        new HDWalletProvider(privateKey, `https://kovan5.arbitrum.io/rpc`),
+      network_id: "*"
+    },
 
-
-      devSkaleLocalMainnet: {
-        provider: () => new HDWalletProvider('0x7ec3bb27ea4718da58251e7f5c2edda830de602ecc30c57efe74fe5c1d798a5b', 'http://127.0.0.1:8545'),
-        network_id: "*",
-        gas: 40000000
-      },
-      devSkaleLocalSideChain: {
-        provider: () => new HDWalletProvider('0x7ec3bb27ea4718da58251e7f5c2edda830de602ecc30c57efe74fe5c1d798a5b', 'http://127.0.0.1:15000'),
-        gasPrice: 0,
-        gas: 40000000,
-        network_id: "*"
-      },
+    devSkaleLocalMainnet: {
+      provider: () =>
+        new HDWalletProvider(
+          "0x7ec3bb27ea4718da58251e7f5c2edda830de602ecc30c57efe74fe5c1d798a5b",
+          "http://127.0.0.1:8545"
+        ),
+      network_id: "*",
+      gas: 40000000
+    },
+    devSkaleLocalSideChain: {
+      provider: () =>
+        new HDWalletProvider(
+          "0x7ec3bb27ea4718da58251e7f5c2edda830de602ecc30c57efe74fe5c1d798a5b",
+          "http://127.0.0.1:15000"
+        ),
+      gasPrice: 0,
+      gas: 40000000,
+      network_id: "*"
+    },
+    kintsugi: {
+      //eth2 merge testnet
+      provider: () =>
+        new HDWalletProvider(privateKey, "https://rpc.kintsugi.themerge.dev"),
+      network_id: "*"
+    }
     // Useful for private networks
     // private: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-      // network_id: 2111,   // This network is yours, in the cloud.
-      // production: true    // Treats this network as if it was a public net. (default: false)
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
     // }
   },
 
@@ -147,15 +192,16 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.6.10",    // Fetch exact version from solc-bin (default: truffle's version)
-       docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
-       settings: {          // See the solidity docs for advice about optimization and evmVersion
+      version: "0.6.10", // Fetch exact version from solc-bin (default: truffle's version)
+      docker: false, // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
           runs: 200
-        },
+        }
         //evmVersion: "byzantium"
-       }
+      }
     }
   }
-}
+};
